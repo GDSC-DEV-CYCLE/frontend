f973a130db9daa2f80b6e380ac3f6cca38adcefd
KimGaeun <greencloud11@hanyang.ac.kr>
2023-01-31 20:46:19 +0900

    💄 Update theme files

---
+++ src/styles/common.ts
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+export const Display1 = css`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const Body2 = css`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const Body1 = css`
+  font-size: 14px;
+  font-weight: 400;
+`;
