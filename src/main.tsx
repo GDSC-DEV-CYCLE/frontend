a51d13527794f1de464d41d8d085e3dc6aabca63
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-01 17:13:18 +0900

    ✨ Add ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
