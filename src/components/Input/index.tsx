560489ee779ec56a975baa7815364f56d7852524
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-02 11:42:20 +0900

    💄 Make Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,1 +1,9 @@
-export {};
+import { InputHTMLAttributes } from 'react';
+
+import * as S from './styled';
+
+type Props = InputHTMLAttributes<HTMLInputElement>;
+
+export default function Input({ ...args }: Props) {
+  return <S.Input {...args} />;
+}
