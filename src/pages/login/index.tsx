d2aa9667aa682ef472f0303727b6b558e9db4595
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-09 01:37:42 +0900

    🔥 Remove useless codes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,3 @@
-import WritingList from '../../components/WritingList';
-
 export default function LoginPage() {
-  return (
-    <>
-      <WritingList
-        title={'제목'}
-        content={
-          'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas...Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas...Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas'
-        }
-        tagList={['개발자', '디자이너']}
-        writer={'김가은'}
-        company={'네이버'}
-        commentNum={0}
-        heartNum={0}
-      />
-    </>
-  );
+  return <>로그인페이지</>;
 }
