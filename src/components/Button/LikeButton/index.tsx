61eb09dbaa1be215cfcd4e40ab488fdcc8342212
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-14 01:44:43 +0900

    🩹 Edit LikeButton props name

--- src/components/Button/LikeButton/index.tsx
+++ src/components/Button/LikeButton/index.tsx
@@ -4,13 +4,13 @@ import * as S from './styled';
 
 type Props = {
   children: string;
-  isSelected: boolean;
+  isClicked: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function LikeButton({ children, isSelected, ...args }: Props) {
+export default function LikeButton({ children, isClicked, ...args }: Props) {
   return (
-    <S.Button {...args} isSelected={isSelected}>
-      <img src={`icons/${isSelected ? 'white' : 'gray'}-heart.svg`} alt="heart icon" />
+    <S.Button {...args} isSelected={isClicked}>
+      <img src={`icons/${isClicked ? 'white' : 'gray'}-heart.svg`} alt="heart icon" />
       <S.NumberWrapper> {children}</S.NumberWrapper>
     </S.Button>
   );
