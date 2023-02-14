5700fc450dbd588edf267e808ee7f0df0d6a9490
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-14 19:02:09 +0900

    💄 Make CommentContainer Component

--- src/pages/postlist/index.tsx
+++ src/pages/postlist/index.tsx
@@ -1,12 +1,3 @@
-import { useState } from 'react';
-
-import LikeButton from '../../components/Button/LikeButton';
-
 export default function PostListPage() {
-  const [a, seta] = useState(false);
-  return (
-    <>
-      <LikeButton isSelected={a}>1.3k</LikeButton>
-    </>
-  );
+  return <></>;
 }
